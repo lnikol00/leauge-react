511fde1e6ef6ddd0d73b696e26288b4747e80f8f
Koce1 <122328343+lnikol00@users.noreply.github.com>
2023-06-24 19:54:59 +0200

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,44 @@
+import React from "react"
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function Header() {
+    return (
+        <Navbar expand="lg" className="bg-body-tertiary" bg='dark' data-bs-theme="dark">
+            <Container>
+                <Navbar.Brand href={`/leauges/${1}`} className='d-flex align-items-center'>
+                    <img
+                        alt=""
+                        src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOAAAADhCAMAAADmr0l2AAAAS1BMVEUYGRrZ2MwAAAAVFRYMDAzi4dUSFBUABQkICg3f3tFubmh9fXYAAAOPjoecm5MXGBmJiIFjY17o59oAAAhTU1CqqaB5eXKdnZW1tKqw/VuyAAABV0lEQVR4nO3dzVKDMBiGUUQLKVVpS/25/yu1axdhHEzyiedcAc/A7p2E7nHnOgAAAAAAAAAAAAB+KI3NpCp9l1MzlwqFc7oem7mmuXzgcHho5jAIFChQoECBAgUKFChQoECBAgUKFChQoECBAgV+s/Px5fj2Us77WLxv/Q32/XMxNQbQ9cDyX1FRAgUGJ1BgcAIFBidQYHACBQYnUGBwAgUGJ1BgcAIFBrd2dmljYOpzKoxL90eYzjnbDoil6TXnVKdwydnUN/f5+bTGAFrUPTCrxoRdlECBwQkUGJxAgcEJFBicQIHBCRQYnECBwQkUGJxAgb9hyS5cGwUIXM4f2Ylrk8/2gfNQ8vLGfF+QA5IFCRQoUKBAgQIFChQoUKBAgQIFChQoUKBAgQL/X+DY8s9ZY4X5LN2mZm5V/u42pGaGGn0AAAAAAAAAAAAA/ClPO9eVvL8ogi9JiniwPlFIzQAAAABJRU5ErkJggg=="
+                        width="35"
+                        height="35"
+                    />{' '}
+                    <h3 className='ps-3 pt-2'>Liga Petice</h3>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav" className='justify-content-end'>
+                    <Nav variant="underline" defaultActiveKey="link-1">
+                        <Nav.Item>
+                            <Nav.Link eventKey="link-1" href={`/leauges/${1}`}>Premier Leauge</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link eventKey="link-2" href={`/leauges/${2}`}>Bundesliga</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link eventKey="link-3" href={`/leauges/${3}`}>La Liga</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link eventKey="link-4" href={`/leauges/${4}`}>Ligue 1</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link eventKey="link-5" href={`/leauges/${5}`}>Seria A</Nav.Link>
+                        </Nav.Item>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+}
+
+export default Header
---
+++ src/components/Tablepage.tsx
@@ -0,0 +1,87 @@
+import React, { useEffect, useState } from 'react'
+import { useParams } from "react-router-dom"
+import Table from 'react-bootstrap/Table'
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Dropdown from 'react-bootstrap/Dropdown';
+
+type leaugesData = {
+    id: number,
+    leauge: leaugeData[],
+}
+
+type leaugeData = {
+    id: number,
+    season: string
+    team: string,
+    played: string,
+    winns: string,
+    ties: string,
+    loses: string,
+    goals: string,
+    points: string
+}
+
+function Tablepage() {
+
+    const params = useParams();
+
+    const [leauge, setLeauge] = useState<leaugesData>();
+
+    useEffect(() => {
+        fetch(`http://localhost:3500/leauges/${params.id}`)
+            .then((res) => res.json())
+            .then((json) => setLeauge(json))
+    })
+
+    return (
+        <div className='bg-secondary p-3'>
+            <div className='d-flex align-items-center pb-2'>
+                <h3>Sezona: </h3>
+                <Dropdown as={ButtonGroup} className='ps-2'>
+                    <Button variant='secondary'>23/24</Button>
+
+                    <Dropdown.Toggle split variant="secondary" id="dropdown-split-basic" />
+
+                    <Dropdown.Menu>
+                        <Dropdown.Item href="#/action-1">22/23</Dropdown.Item>
+                        <Dropdown.Item href="#/action-2">21/22</Dropdown.Item>
+                        <Dropdown.Item href="#/action-3">20/21</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+            </div>
+            <Table striped bordered hover variant="dark" responsive>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Tim</th>
+                        <th>Odigrano susreta</th>
+                        <th>Pobjeda</th>
+                        <th>Nerješeno</th>
+                        <th>Porazi</th>
+                        <th>Gol razlika</th>
+                        <th>Bodovi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {leauge?.leauge.map((season, index) => {
+                        return (
+                            <tr key={index}>
+                                <td>{index + 1}</td>
+                                <td>{season.team}</td>
+                                <td>{season.played}</td>
+                                <td>{season.winns}</td>
+                                <td>{season.ties}</td>
+                                <td>{season.loses}</td>
+                                <td>{season.goals}</td>
+                                <td>{season.points}</td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </Table >
+        </div >
+    );
+}
+
+export default Tablepage
