acd80bbeabe82c79ec3211b5f93a58c12c033f83
Koce1 <122328343+lnikol00@users.noreply.github.com>
2023-06-25 12:04:21 +0200

    adding home page

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import "./App.css"
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Tablepage from "./components/Tablepage";
+import Home from "./components/Home";
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
         <Header />
         <div className="main-container">
           <Routes>
+            <Route index element={<Home />} />
             <Route path="leauges/:id" element={<Tablepage />} />
           </Routes>
         </div>
